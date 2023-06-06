a0739f770ae2f7da2dda136f1ce502c7e2deffdc
Fabio Pellegrini <fabiopellegrini90@gmail.com>
2023-06-06 11:55:17 +0000

    ci: fix runner version

--- mix.exs
+++ mix.exs
@@ -6,7 +6,7 @@ defmodule GoalSeek.MixProject do
     [
       app: :goal_seek,
       version: "0.1.0",
-      elixir: "~> 1.14",
+      elixir: "~> 1.12",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       test_coverage: [tool: ExCoveralls],
